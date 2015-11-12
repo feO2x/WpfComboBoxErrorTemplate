f17178f57dc90bed2d7327d544c47cf3bb543b3e
Kenny Pflug <kenny.pflug@live.de>
2015-11-12 13:09:59 +0100

    added example for custom validation rules

---
+++ WpfComboBoxErrorTemplate/ComboBoxWithCustomValidationRule.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfComboBoxErrorTemplate
+{
+    public partial class ComboBoxWithCustomValidationRule : UserControl
+    {
+        public IReadOnlyList<string> Items { get; private set; }
+        public string SelectedItem { get; set; }
+
+        public ComboBoxWithCustomValidationRule()
+        {
+            Items = Model.CreateItems();
+            InitializeComponent();
+            CustomValidationRule.IsValid = true;
+        }
+
+        private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (CustomValidationRule == null) return;
+            CustomValidationRule.IsValid = true;
+        }
+
+        private void ToggleButton_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            if (CustomValidationRule == null) return;
+            CustomValidationRule.IsValid = false;
+        }
+    }
+}
---
+++ WpfComboBoxErrorTemplate/CustomValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace WpfComboBoxErrorTemplate
+{
+    public sealed class CustomValidationRule : ValidationRule
+    {
+        public bool IsValid;
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (IsValid) return ValidationResult.ValidResult;
+
+            return new ValidationResult(false, "Something went wrong");
+        }
+    }
+}
